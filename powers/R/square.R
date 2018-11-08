f8e881d4033863d50145fa07fb36df73584ce6ed
xcct2012 <42976541+xcct2012@users.noreply.github.com>
2018-11-08 11:17:02 -0800

    Upload cm105-106 particpation

---
+++ powers/R/square.R
@@ -0,0 +1,27 @@
+#'square anything
+#'
+#'@param x
+#'@return square of any integer
+#'@export square
+#'
+#'
+#'
+#'
+#'
+
+square <- function(x){
+  return(x^2)
+}
+pow <- function(x, a=2) x^a
+#'Cube any #
+#'
+#'@param x
+#'@return cube of any integer
+#'@export cube
+#'
+#'
+#'
+#'
+cube<- function(x){
+  pow(x,a=3)
+}
